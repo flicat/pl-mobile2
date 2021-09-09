65b30da1d54c193b9a99b24876f56d16ef6b078c
liyuelong <1393926171@qq.com>
2021-09-09 10:52:58 +0800

    fetch组件修改为可以直接导出方法

--- packages/fetch/index.js
+++ packages/fetch/index.js
@@ -7,153 +7,156 @@ import polyfill from './polyfill-patch-fetch'
 import request from './fetch'
 import { is } from '../../src/assets/utils'
 
-export default function (App) {
-  polyfill()
+const beforeHandlerArr = []
 
-  const beforeHandlerArr = []
+const afterHandlerArr = []
 
-  const afterHandlerArr = []
-
-  const handler = async function (option) {
-    for (let i = beforeHandlerArr.length; i--;) {
-      beforeHandlerArr[i](option)
-    }
-
-    let response = request(option)
-
-    for (let i = afterHandlerArr.length; i--;) {
-      await afterHandlerArr[i](response)
-    }
-    return response
+const handler = async function (option) {
+  for (let i = beforeHandlerArr.length; i--;) {
+    beforeHandlerArr[i](option)
   }
 
-  class Fetch {
-    constructor(option) {
-      if (is(option, 'object')) {
-        this.option = option
-      }
-    }
-    options(option) {
-      if (is(option, 'object')) {
-        Object.assign(this.option, option)
-      }
-      return this
-    }
-    url(...args) {
-      this.option.url = [this.option.url, ...args].join('/')
-      return this
-    }
-    headers(headers) {
-      if (!is(this.option.headers, 'object')) {
-        this.option.headers = {}
-      }
-      if (is(headers, 'object')) {
-        Object.assign(this.option.headers, headers)
-      }
-      return this
-    }
-    request(option) {
-      return handler(Object.assign({}, this.option, option))
-    }
-    get(url, data, option) {
-      let getOption = {
-        method: 'get'
-      }
-      if (url) {
-        getOption.url = url
-      }
-      if (data) {
-        getOption.data = data
-      }
-      return handler(Object.assign({}, this.option, option, getOption))
-    }
-    post(url, data, option) {
-      let postOption = {
-        method: 'post'
-      }
-      if (url) {
-        postOption.url = url
-      }
-      if (data) {
-        postOption.data = data
-      }
-      return handler(Object.assign({}, this.option, option, postOption))
-    }
+  let response = request(option)
+
+  for (let i = afterHandlerArr.length; i--;) {
+    await afterHandlerArr[i](response)
   }
-
-  App.config.globalProperties.$fetch = new Proxy(handler, {
-    get(target, propKey, receiver) {
-      switch (propKey) {
-        case 'options':
-          return function (option) {
-            return new Fetch(option)
-          }
-
-        case 'url':
-          return function (...args) {
-            const target = new Fetch({})
-            target.url(...args)
-            return target
-          }
-
-        case 'headers':
-          return function (headers) {
-            const target = new Fetch({})
-            target.headers(headers)
-            return target
-          }
-
-        case 'request':
-          return function (option) {
-            return handler(option)
-          }
-
-        case 'get':
-          return function (url, data, option) {
-            let getOption = {
-              method: 'get'
-            }
-            if (url) {
-              getOption.url = url
-            }
-            if (data) {
-              getOption.data = data
-            }
-            return handler(Object.assign({}, option, getOption))
-          }
-
-        case 'post':
-          return function (url, data, option) {
-            let postOption = {
-              method: 'post'
-            }
-            if (url) {
-              postOption.url = url
-            }
-            if (data) {
-              postOption.data = data
-            }
-            return handler(Object.assign({}, option, postOption))
-          }
-
-        case 'before':
-          return function (func) {
-            if (is(func, 'function')) {
-              beforeHandlerArr.push(func)
-            }
-            return receiver
-          }
-
-        case 'after':
-          return function (func) {
-            if (is(func, 'function')) {
-              afterHandlerArr.push(func)
-            }
-            return receiver
-          }
-      }
-
-      return Reflect.get(target, propKey, receiver);
-    }
-  })
+  return response
 }
+
+class Fetch {
+  constructor(option) {
+    if (is(option, 'object')) {
+      this.option = option
+    }
+  }
+  options(option) {
+    if (is(option, 'object')) {
+      Object.assign(this.option, option)
+    }
+    return this
+  }
+  url(...args) {
+    this.option.url = [this.option.url, ...args].join('/')
+    return this
+  }
+  headers(headers) {
+    if (!is(this.option.headers, 'object')) {
+      this.option.headers = {}
+    }
+    if (is(headers, 'object')) {
+      Object.assign(this.option.headers, headers)
+    }
+    return this
+  }
+  request(option) {
+    return handler(Object.assign({}, this.option, option))
+  }
+  get(url, data, option) {
+    let getOption = {
+      method: 'get'
+    }
+    if (url) {
+      getOption.url = url
+    }
+    if (data) {
+      getOption.data = data
+    }
+    return handler(Object.assign({}, this.option, option, getOption))
+  }
+  post(url, data, option) {
+    let postOption = {
+      method: 'post'
+    }
+    if (url) {
+      postOption.url = url
+    }
+    if (data) {
+      postOption.data = data
+    }
+    return handler(Object.assign({}, this.option, option, postOption))
+  }
+}
+
+const fetchProxy = new Proxy(handler, {
+  get(target, propKey, receiver) {
+    switch (propKey) {
+      case 'options':
+        return function (option) {
+          return new Fetch(option)
+        }
+
+      case 'url':
+        return function (...args) {
+          const target = new Fetch({})
+          target.url(...args)
+          return target
+        }
+
+      case 'headers':
+        return function (headers) {
+          const target = new Fetch({})
+          target.headers(headers)
+          return target
+        }
+
+      case 'request':
+        return function (option) {
+          return handler(option)
+        }
+
+      case 'get':
+        return function (url, data, option) {
+          let getOption = {
+            method: 'get'
+          }
+          if (url) {
+            getOption.url = url
+          }
+          if (data) {
+            getOption.data = data
+          }
+          return handler(Object.assign({}, option, getOption))
+        }
+
+      case 'post':
+        return function (url, data, option) {
+          let postOption = {
+            method: 'post'
+          }
+          if (url) {
+            postOption.url = url
+          }
+          if (data) {
+            postOption.data = data
+          }
+          return handler(Object.assign({}, option, postOption))
+        }
+
+      case 'before':
+        return function (func) {
+          if (is(func, 'function')) {
+            beforeHandlerArr.push(func)
+          }
+          return receiver
+        }
+
+      case 'after':
+        return function (func) {
+          if (is(func, 'function')) {
+            afterHandlerArr.push(func)
+          }
+          return receiver
+        }
+    }
+
+    return Reflect.get(target, propKey, receiver);
+  }
+})
+
+fetchProxy.install = function (App) {
+  polyfill()
+  App.config.globalProperties.$fetch = fetchProxy
+}
+
+export default fetchProxy
