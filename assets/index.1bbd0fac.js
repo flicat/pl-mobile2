df48ae53519b4e4df1cde916adfab21739e39da3
flicat <flicat@users.noreply.github.com>
2022-02-21 08:11:05 +0000

    deploy: 17e41694a0ad214dcc96f14a22041af1c397be9b

--- assets/index.1bbd0fac.js
+++
@@ -1,1 +0,0 @@
-import{r,o,c as e}from"./vendor.6f33ac2d.js";const t={};t.render=function(t,n){const s=r("router-view");return o(),e(s)};export{t as default};
---
+++ assets/index.b9af579d.js
@@ -0,0 +1,1 @@
+import{r,o,c as e}from"./vendor.c07f43a6.js";const t={};t.render=function(t,n){const s=r("router-view");return o(),e(s)};export{t as default};
